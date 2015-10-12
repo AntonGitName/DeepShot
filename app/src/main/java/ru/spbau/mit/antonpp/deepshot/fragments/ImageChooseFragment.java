1854a4f77228beb797f75f0177b8fdfac782b605
Anton <mordbergak@gmail.com>
2015-10-12 14:28:09 +0300

    Taking camera shots updated

--- app/src/main/java/ru/spbau/mit/antonpp/deepshot/fragments/ImageChooseFragment.java
+++ app/src/main/java/ru/spbau/mit/antonpp/deepshot/fragments/ImageChooseFragment.java
@@ -11,12 +11,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ru.spbau.mit.antonpp.deepshot.R;
 
@@ -56,22 +60,24 @@ public class ImageChooseFragment extends DialogFragment {
         builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
                 if (item == 0) {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File file = new File(Environment.getExternalStorageDirectory(),
-                            "tmp_image_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
-
-                    cameraImageUri = Uri.fromFile(file);
-
-                    try {
-                        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, cameraImageUri);
-                        intent.putExtra("return-data", true);
-
-                        activity.startActivityForResult(intent, PICK_FROM_CAMERA);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    // Ensure that there's a camera activity to handle the intent
+                    if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+                        // Create the File where the photo should go
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                            // Error occurred while creating the File
+                            Log.e(TAG, "Camera failed");
+                        }
+                        // Continue only if the File was successfully created
+                        if (photoFile != null) {
+                            cameraImageUri = Uri.fromFile(photoFile);
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+                            activity.startActivityForResult(takePictureIntent, PICK_FROM_CAMERA);
+                        }
                     }
-
-                    dialog.cancel();
                 } else {
                     Intent intent = new Intent();
 
@@ -83,6 +89,21 @@ public class ImageChooseFragment extends DialogFragment {
             }
         });
         return builder.create();
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+
+        // Save a file: path for use with ACTION_VIEW intents
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
     }
 
     @Override
