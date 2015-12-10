611c33f75cc4a8e75d1c326058f3ff9e8a04b5ee
Anton <mordbergak@gmail.com>
2015-12-10 17:06:50 +0300

    UI updated.

--- app/src/main/java/ru/spbau/mit/antonpp/deepshot/fragment/ChooseStyleFragment.java
+++ app/src/main/java/ru/spbau/mit/antonpp/deepshot/fragment/ChooseStyleFragment.java
@@ -28,6 +28,6 @@ public class ChooseStyleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_filter, container, false);
+        return inflater.inflate(R.layout.fragment_choose_style, container, false);
     }
 }
---
+++ app/src/main/java/ru/spbau/mit/antonpp/deepshot/view/NonSwipeableViewPager.java
@@ -0,0 +1,34 @@
+package ru.spbau.mit.antonpp.deepshot.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * @author antonpp
+ * @since 10/12/15
+ */
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
+
