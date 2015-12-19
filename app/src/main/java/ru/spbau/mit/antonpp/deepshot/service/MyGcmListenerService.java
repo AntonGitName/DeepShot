612574e72f9de5b3e4243cf6026fd059ea4584b7
Anton <mordbergak@gmail.com>
2015-12-19 15:48:00 +0300

    Gcm message title changed.

--- app/src/main/java/ru/spbau/mit/antonpp/deepshot/service/MyGcmListenerService.java
+++ app/src/main/java/ru/spbau/mit/antonpp/deepshot/service/MyGcmListenerService.java
@@ -72,7 +72,7 @@ public class MyGcmListenerService extends GcmListenerService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("GCM Message")
+                .setContentTitle(getString(R.string.app_name))
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
